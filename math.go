582a5dc96c2860785743ef87d9665bab7893d52b
Matt Knopp <matt.knopp@gmail.com>
2013-08-16 15:24:09 -0700

    go 1.0 compat

--- math.go
+++ math.go
@@ -3,16 +3,14 @@ package main
 func IntMax (x, y int64) int64 {
 	if (x > y) {
 		return x
-	} else {
-		return y
-	}
+	} 
+	return y
 }
 
 func IntMin(x, y int64) int64 {
 	if (x < y) {
 		return x
-	} else {
-		return y
-	}
+	}	
+	return y
 }
 
