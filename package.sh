3199682508189311dc4cb8bdd8310fb083e50b39
Matt Knopp <mknopp@ip-10-180-1-243.aws-vagrant-us-west.yammer.com>
2013-08-22 22:47:06 +0000

    upstart and packginging

--- package.sh
+++ package.sh
@@ -7,7 +7,7 @@ mkdir -p $TEMP/pkg
 
 GOBIN=$TEMP/bin GOPATH=$TEMP go get github.com/mhat/mikkin_overwatch
 
-VERSION=1.0
+VERSION=1.0-2
 
 #`$TEMP/bin/uniqush-push --version | sed 's/uniqush-push //'`
 
@@ -15,6 +15,7 @@ BUILD=`pwd`/mikkin-overwatch-$VERSION
 mkdir -p $BUILD/opt/mikkin_overwatch/bin
 mkdir -p $BUILD/opt/mikkin_overwatch/assets
 mkdir -p $BUILD/opt/mikkin_overwatch/templates
+mkdir -p $BUILD/etc/init
 mkdir -p $BUILD/etc/mikkin_overwatch/
 mkdir -p $BUILD/etc/mikkin_overwatch/dropwizard.d/
 
@@ -22,9 +23,11 @@ ARCH=`uname -m`
 
 cp $TEMP/src/github.com/mhat/mikkin_overwatch/LICENSE $LICENSE
 cp $TEMP/bin/mikkin_overwatch $BUILD/opt/mikkin_overwatch/bin
-cp $TEMP/src/github.com/mhat/mikkin_overwatch/config/yammer-development-environment.json $BUILD/etc/mikkin_overwatch/server.json
 cp -R $TEMP/src/github.com/mhat/mikkin_overwatch/assets $BUILD/opt/mikkin_overwatch
 cp -R $TEMP/src/github.com/mhat/mikkin_overwatch/templates $BUILD/opt/mikkin_overwatch
+cp $TEMP/src/github.com/mhat/mikkin_overwatch/config/yammer-development-environment.json $BUILD/etc/mikkin_overwatch/server.json
+cp $TEMP/src/github.com/mhat/mikkin_overwatch/config/upstart/mikkin_overwatch.conf $BUILD/etc/init/mikkin_overwatch.conf
+
 
 #fpm --verbose -s dir -t rpm -v $VERSION -n mikkin_overwatch --license=$LICENSE --maintainer="Matt Knopp" --vendor "Yammer" --description "Yammer Development Environment Log Tailer" -a $ARCH -C $BUILD .
 fpm --verbose -s dir -t deb -v $VERSION -n mikkin_overwatch --license=$LICENSE --maintainer="Matt Knopp" --vendor "Yammer" --description "Yammer Development Environment Log Tailer" -a $ARCH -C $BUILD .
